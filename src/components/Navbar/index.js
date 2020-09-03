b4d0ee8d575aea700aca5c6511fae1a642f348aa
Akeem Flavors <akeemflavors@gmail.com>
2020-09-02 23:51:44 -0400

    Employee directory Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+function Navbar() {
+    return (
+        <div>
+            <div className="ui container">
+              <h1>Employee Directory</h1>  
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
