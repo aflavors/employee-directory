019f910816d06cd90d621b87d1f7ca941a6f264f
Akeem Flavors <akeemflavors@gmail.com>
2020-09-05 15:15:04 -0400

    Axios call for 50 users/employees from randomuser.me

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends Component {
+
+  componentDidMount(){
+    axios
+      .get("https://randomuser.me/api/?results=50")
+      .then(res => console.log(res.data));
+  }
+
+  render(){
+
+    return (
     <div>
       <Navbar />
     </div>
   );
+  }
 }
 
 export default App;
