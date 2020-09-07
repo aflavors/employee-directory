486f38dd3abd220eaf5d03fe220941d93689f86e
Akeem Flavors <akeemflavors@gmail.com>
2020-09-07 13:16:15 -0400

    Added Header element and Icon to Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import "./style.css";
 
+import { Header, Icon } from 'semantic-ui-react'
+
 function Navbar() {
     return (
         <div>
-            <div className="ui container">
-              <h1>Employee Directory</h1>  
-            </div>
+            <Header as='h2' icon textAlign='center'>
+                <Icon name='users' circular />
+                <Header.Content>
+                    Employee Directory
+                </Header.Content>
+                <span className="sub header" textAlign="center">
+                    Click on carrots to filter by name or use the search box to narrow your results.
+                </span>   
+            </Header>
         </div>
     )
 }
